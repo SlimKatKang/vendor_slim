03e5e351e893365739ffe3915bf2cae5fe770596
aikons <dugujianke@live.com>
2013-12-15 16:07:19 +0800

    phoneloc support

--- config/common.mk
+++ config/common.mk
@@ -45,6 +45,10 @@ PRODUCT_COPY_FILES += \
 
 #LOCAL SLIM CHANGES  - END
 
+# Copy phoneloc files
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/common/media/mokee-phoneloc.dat:system/media/mokee-phoneloc.dat
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
